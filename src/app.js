ebd5913e76031585bbfb4a2c229f3fb403daeedf
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-04-10 01:07:07 -0300

    Data storage and format of user and tweet added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors"
 
 const app = express()
+app.use((cors))
+
+
+const user = [{
+	username: 'bobesponja', 
+	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
+}]
+
+const tweets = [
+    {
+        username: "bobesponja",
+      tweet: "Eu amo hambúrguer de siri!"
+    }
+]
+
 
 const PORT = 5000
 
