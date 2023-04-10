22fd6a61fc865899f79acb98f132cc5aad1651c5
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-04-10 02:04:30 -0300

    get tweet route ok

--- src/app.js
+++ src/app.js
@@ -2,22 +2,22 @@ import express from "express";
 import cors from "cors"
 
 const app = express()
-app.use((cors))
+app.use(cors())
 
 
-const user = [{
-	username: 'bobesponja', 
-	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-}]
 
-const tweets = [
-    {
-        username: "bobesponja",
-      tweet: "Eu amo hambúrguer de siri!"
-    }
-]
+app.get("/tweets", (req, res) => {
+    const tweet = [
+        {
+            username: "bobesponja",
+            avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+            tweet: "Eu amo hambúrguer de siri!"
+        }
+    ]
+    res.send(tweet)
+})
 
 
 const PORT = 5000
 
-app.listen(PORT, () => console.log(`Servidor rodando no server ${PORT}` ))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}` ))
